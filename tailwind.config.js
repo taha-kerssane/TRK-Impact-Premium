00847452e7af8a3ba447bf6f03faa49db4eac0aa
Site Web TRK-Impact <tahakerssanepro@gmail.com>
2025-10-07 16:44:51 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ink: '#0f172a',
+        gold: '#c6a972'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
