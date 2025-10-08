3f38fa05c6d62fb369fa50719fe0672deeacf3d9
Site Web TRK-Impact <tahakerssanepro@gmail.com>
2025-10-08 12:53:46 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css"; // garde-le si tu as un fichier de styles globaux
 
-createRoot(document.getElementById('root')).render(<App />)
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
